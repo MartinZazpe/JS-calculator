759434036b85a87f3f675fd34ca9f812efeddf39
MartinZazpe <martinzazpe1@gmail.com>
2022-11-26 14:47:10 +0100

    something

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./api/index')
+var indexRouter = require('./api/mainRoute')
 var usersRouter = require('./routes/users')
 
 var app = express()
